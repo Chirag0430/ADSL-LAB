472dd36d3dd94a8a0551c8c647e1e40723c7528d
Chirag0430 <101520308+Chirag0430@users.noreply.github.com>
2023-05-03 02:16:38 +0530

    Update Assignment_7_PrimsAlgo.py

--- Assignment_7_PrimsAlgo.py
+++ Assignment_7_PrimsAlgo.py
@@ -1,31 +1,31 @@
-INF=99
+INF = 99
 
-N=5
+nodes = int(input("Enter the number of nodes: "))
 
-G=[[0,19,5,0,0],
-   [19,0,5,9,2],
-   [5,5,0,1,6],
-   [0,9,1,0,1],
-   [0,2,6,1,0]]
+graph = []
+print("Enter the graph row wise :")
+for i in range(nodes):
+    row = list(map(int, input().split()))
+    graph.append(row)
 
-visited_node = [0,0,0,0,0]
-no_edge=0
-visited_node[0]= True
+visited = [0] * nodes
+no_edge = 0
+visited[0] = True
 
 print("Edge : Weight\n")
-while (no_edge < N - 1):
+while (no_edge < nodes - 1):
     minimum = INF
     a = 0
     b = 0
-    for m in range(N):
-        if visited_node[m]:
-            for n in range(N):
-                if ((not visited_node[n]) and G[m][n]):
+    for m in range(nodes):
+        if visited[m]:
+            for n in range(nodes):
+                if ((not visited[n]) and graph[m][n]):
 
-                    if minimum > G[m][n]:
-                        minimum = G[m][n]
+                    if minimum > graph[m][n]:
+                        minimum = graph[m][n]
                         a = m
                         b = n
-    print(str(a) + "-" + str(b) + ":" + str(G[a][b]))
-    visited_node[b] = True
+    print(str(a) + "-" + str(b) + ":" + str(graph[a][b]))
+    visited[b] = True
     no_edge += 1
